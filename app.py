6fca203f5def8a3cf2f66ed3394e0861822b2eab
reubenjue <rnjue30@gmail.com>
2026-03-06 16:45:43 +0100

    Update app.py

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ import streamlit as st
 # ============================================
 
 st.set_page_config(
-    page_title="DNA Cloning Calculator | Njue BioTools",
+    page_title="DNA Cloning Calculator | RNMolBioTools",
     page_icon="🧬",
     layout="wide"
 )
